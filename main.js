334dbd5462245aa86bb236ed83428313af94935a
jaweria455 <161853228+jaweria455@users.noreply.github.com>
2024-03-01 20:14:19 +0500

    quest 2

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//assignment -1
+// storing the person name  in a variable
+let personName = "Jaweria";
+// printing a message to the person
+console.log('hello ${personName} would you like to learn some python today?');
