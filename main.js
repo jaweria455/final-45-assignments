b7f703038b50b136abd0e87ae378eaec0b13343b
jaweria455 <161853228+jaweria455@users.noreply.github.com>
2024-03-01 20:26:26 +0500

    ques 2

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 "use strict";
-//assignment -1
-// storing the person name  in a variable
-let personName = "Jaweria";
-// printing a message to the person
-console.log('hello ${personName} would you like to learn some python today?');
+//storing a person name in a variable
+let personName = "Jaweria Mubin";
+//printing a person name in lowercase
+console.log("lowercase;", personName.toLowerCase());
+//printing a person name in uppercase
+console.log("Uppercase:", personName.toUpperCase());
+//printing a person name in titlecase
+console.log("Titlecase:", personName.toTitleCase());
